19579ba8cf396182e9ac288e0917035995d60715
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-08 17:59:53 +0300

    коммит lessom06

--- script.js
+++ script.js
@@ -2,29 +2,28 @@
 
 
 function myNumber () {
-    return function (num) {
         return Math.ceil((Math.random() * 100));
     };
-};
+
 
 const number = myNumber();
 
-console.log(number());
+console.log(number)
 
-// const ask = +prompt("Угадайте число от 1 до 100")
+
 const game = function(n){
-    n = +prompt("Угадайте число от 1 до 100")
+      n = +prompt("Угадайте число от 1 до 100")
     if (n == "" || n == null ) {
         alert("введите число !!!")
         game(n)
-    }  if(n > number()) {
+    }  if(n > number) {
         alert("ваше число больше загаданного"); 
         game(n)
-    }  if (n < number()) {
+    }  if (n < number) {
         alert("ваше число меньше загаданного") ;
         game(n)
-    } if (n === number()) {
-        alert("Ура вы победили!!!")
+    } if (n == number) {
+        alert("Ура вы победили")
     }
 }
  game()
