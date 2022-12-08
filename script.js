7080dfd56ea3bcfdffb1bf99b3f13507b99dc9e3
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-08 14:43:56 +0300

    посмотри плиз

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+"use strict";
 
 
+function myNumber () {
+    return function (num) {
+        return Math.ceil((Math.random() * 100));
+    };
+};
+
+const number = myNumber();
+
+console.log(number());
+
+// const ask = +prompt("Угадайте число от 1 до 100")
+const game = function(n){
+    n = +prompt("Угадайте число от 1 до 100")
+    if (n == " " || n == null ) {
+        +prompt("Угадайте число от 1 до 100")
+        game(n)
+    } else if(n > number()) {
+        alert("ваше число больше загаданного"); continue
+    } else if (n < number()) {
+        alert("ваше число меньше загаданного") ;
+    }
+}
+ game()
