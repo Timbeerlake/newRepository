bac36c3d0c0b69e068b28b990cbb05cb5aec7a10
Temur Meiliev <temur.meiliev@mail.ru>
2022-12-17 13:06:46 +0500

    комит 10 урок

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-"use strict";
+const allBooks = document.querySelectorAll(".books")
+const book = document.querySelectorAll(".book")
+const google = document.querySelectorAll(".adv")
+
+const book2 = document.querySelectorAll(".book")[5]
+const ul = book2.querySelectorAll('li')
+const book3 = document.querySelectorAll(".book")[2]
+const ul2 = book3.querySelectorAll('li')
+
+const newLi = document.createElement("li")
+newLi.textContent = "Глава 8: За пределами ES6";
 
 
-function myNumber () {
-        return Math.ceil((Math.random() * 100));
-    };
+const bookClone = book[4].cloneNode(true)
+const h2 = bookClone.querySelector("a")
+h2.textContent = "Книга 3. this и Прототипы Объектов";
+
+google[0].remove()
+const bgi = document.querySelector('body')
+console.log(bgi)
+bgi.style.backgroundImage = "url('./image/you-dont-know-js.jpg')";
+
+console.log(allBooks);
+console.log(book);
+console.dir(bookClone)
+console.dir(h2)
+console.dir(book2)
+console.dir(ul2)
+console.log(newLi)
 
 
-const number = myNumber();
+book[0].replaceWith(book[1])
+book[2].replaceWith(book[0])
+book[3].replaceWith(book[4])
+book[5].before(book[3])
+book[5].after(book[2])
+book[4].replaceWith(bookClone)
+ul[2].before(ul[9])
+ul[5].before(ul[2])
+ul[8].before(ul[5])
+ul2[8].append(newLi)
 
-console.log(number)
 
 
-const game = function(n){
-      n = +prompt("Угадайте число от 1 до 100")
-    if (n == "" || n == null ) {
-        alert("введите число !!!")
-        game(n)
-    }  if(n > number) {
-        alert("ваше число больше загаданного"); 
-        game(n)
-    }  if (n < number) {
-        alert("ваше число меньше загаданного") ;
-        game(n)
-    } if (n == number) {
-        alert("Ура вы победили")
-    }
-}
- game()
+
